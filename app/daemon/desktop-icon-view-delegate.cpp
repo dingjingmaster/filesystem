5882bfb0e546e50dc2bf780d96484d053ab360d6
dingjingmaster <dingjing@live.cn>
2020-07-05 14:43:11 +0800

    暂存

--- app/daemon/desktop-icon-view-delegate.cpp
+++ app/daemon/desktop-icon-view-delegate.cpp
@@ -1,9 +1,14 @@
 #include "desktop-icon-view-delegate.h"
 #include "desktop-icon-view.h"
 
+#include <QFileInfo>
 #include <QPainter>
 
 #include <delegate/icon-view-delegate.h>
+#include <delegate/icon-view-editor.h>
+
+#include <file/file-operation-manager.h>
+#include <file/file-rename-operation.h>
 
 
 DesktopIconViewDelegate::DesktopIconViewDelegate(QObject *parent) : QStyledItemDelegate(parent)
@@ -18,7 +23,8 @@ DesktopIconViewDelegate::~DesktopIconViewDelegate()
 
 DesktopIconView *DesktopIconViewDelegate::getView() const
 {
-
+    auto view = qobject_cast<DesktopIconView*>(parent());
+    return view;
 }
 
 void DesktopIconViewDelegate::initStyleOption(QStyleOptionViewItem *option, const QModelIndex &index) const
@@ -28,17 +34,58 @@ void DesktopIconViewDelegate::initStyleOption(QStyleOptionViewItem *option, cons
 
 QSize DesktopIconViewDelegate::sizeHint(const QStyleOptionViewItem &option, const QModelIndex &index) const
 {
-
+    auto view = qobject_cast<DesktopIconView*>(parent());
+    auto zoomLevel = view->zoomLevel();
+    switch (zoomLevel) {
+    case DesktopIconView::Small:
+        return QSize(60, 60);
+    case DesktopIconView::Normal:
+        return QSize(90, 90);
+    case DesktopIconView::Large:
+        return QSize(105, 118);
+    case DesktopIconView::Huge:
+        return QSize(120, 140);
+    default:
+        return QSize(90, 90);
+    }
 }
 
 void DesktopIconViewDelegate::setEditorData(QWidget *editor, const QModelIndex &index) const
 {
+    IconViewEditor *edit = qobject_cast<IconViewEditor*>(editor);
+    if (!edit) {
+        return;
+    }
 
+    auto cursor = edit->textCursor();
+    cursor.setPosition(0, QTextCursor::MoveAnchor);
+    cursor.movePosition(QTextCursor::End, QTextCursor::KeepAnchor);
+    if (edit->toPlainText().contains(".") && !edit->toPlainText().startsWith(".")) {
+        cursor.movePosition(QTextCursor::WordLeft, QTextCursor::KeepAnchor, 2);
+    }
+    edit->setTextCursor(cursor);
 }
 
 void DesktopIconViewDelegate::setModelData(QWidget *editor, QAbstractItemModel *model, const QModelIndex &index) const
 {
-
+    IconViewEditor *edit = qobject_cast<IconViewEditor*>(editor);
+    if (!edit) {
+        return;
+    }
+    auto newName = edit->toPlainText();
+    auto oldName = index.data(Qt::DisplayRole).toString();
+    QFileInfo info(index.data().toUrl().path());
+    auto suffix = "." + info.suffix();
+    if (newName.isNull())
+        return;
+    //process special name . or .. or only space
+    if (newName == "." || newName == ".." || newName.trimmed() == "")
+        newName = "";
+    if (newName.length() >0 && newName != oldName && newName != suffix) {
+        auto fileOpMgr = FileOperationManager::getInstance();
+        auto renameOp = new FileRenameOperation(index.data(Qt::UserRole).toString(), newName);
+        fileOpMgr->slotStartOperation(renameOp, true);
+    }
 }
 
 void DesktopIconViewDelegate::paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const
@@ -179,10 +226,55 @@ void DesktopIconViewDelegate::paint(QPainter *painter, const QStyleOptionViewIte
 
 QWidget *DesktopIconViewDelegate::createEditor(QWidget *parent, const QStyleOptionViewItem &option, const QModelIndex &index) const
 {
+    auto edit = new IconViewEditor(parent);
+    auto font = option.font;
+    auto view = qobject_cast<DesktopIconView*>(this->parent());
+//    switch (view->zoomLevel()) {
+//    case DesktopIconView::Small:
+//        font.setPixelSize(int(font.pixelSize() * 0.8));
+//        break;
+//    case DesktopIconView::Large:
+//        font.setPixelSize(int(font.pixelSize() * 1.2));
+//        break;
+//    case DesktopIconView::Huge:
+//        font.setPixelSize(int(font.pixelSize() * 1.4));
+//        break;
+//    default:
+//        break;
+//    }
 
+    edit->setFont(font);
+
+    edit->setContentsMargins(0, 0, 0, 0);
+    edit->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    edit->setMinimumSize(sizeHint(option, index).width(), 54);
+
+    edit->setText(index.data(Qt::DisplayRole).toString());
+    edit->setAlignment(Qt::AlignCenter);
+    QTimer::singleShot(1, [=]() {
+        edit->minimalAdjust();
+    });
+
+    connect(edit, &IconViewEditor::returnPressed, [=]() {
+        this->setModelData(edit, nullptr, index);
+        edit->deleteLater();
+    });
+
+    return edit;
 }
 
 void DesktopIconViewDelegate::updateEditorGeometry(QWidget *editor, const QStyleOptionViewItem &option, const QModelIndex &index) const
 {
+    QStyledItemDelegate::updateEditorGeometry(editor, option, index);
+    auto edit = qobject_cast<IconViewEditor*>(editor);
+    if (!edit) {
+        return;
+    }
 
+    auto opt = option;
+    auto iconExpectedSize = getView()->iconSize();
+
+    edit->move(opt.rect.x(), opt.rect.y() + iconExpectedSize.height());
+
+    edit->resize(edit->document()->size().width(), edit->document()->size().height() + 10);
 }
