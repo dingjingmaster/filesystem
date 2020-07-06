00dcda7f169f8d53c3a0d886cbf24ce8afdbbeac
dingjingmaster <dingjing@live.cn>
2020-07-06 20:49:48 +0800

    count 操作

--- app/file/file-count-operation.cpp
+++ app/file/file-count-operation.cpp
@@ -1,9 +1,22 @@
 #include "file-count-operation.h"
+#include "file-node-reporter.h"
+#include "file-node.h"
 
 
 FileCountOperation::FileCountOperation(const QStringList &uris, bool countRoot, QObject *parent) : FileOperation(parent)
 {
-
+    mCountRoot = countRoot;
+    mReporter = new FileNodeReporter(this);
+    //connect(m_reporter, &FileNodeReporter::nodeFound, this, &FileOperation::operationPreparedOne);
+    connect(mReporter, &FileNodeReporter::nodeFound, [=](const QString &uri, quint64 size) {
+        mFileCount++;
+        if (uri.contains("/.")) {
+            mHiddenFileCount++;
+        }
+        mTotalSize += size;
+        Q_EMIT this->operationPreparedOne(uri, size);
+    });
+    mUris = uris;
 }
 
 FileCountOperation::~FileCountOperation()
@@ -13,7 +26,34 @@ FileCountOperation::~FileCountOperation()
 
 void FileCountOperation::run()
 {
+    Q_EMIT operationStarted();
+    if (mUris.isEmpty()) {
+        Q_EMIT operationFinished();
+    }
 
+    QList<FileNode *> nodes;
+    for (auto uri : mUris) {
+        auto node = new FileNode(uri, nullptr, mReporter);
+        node->findChildrenRecursively();
+        nodes<<node;
+    }
+    if (!this->isCancelled()) {
+        if (!mCountRoot) {
+            for (auto node : nodes) {
+                -- mFileCount;
+                if (node->baseName().startsWith(".")) {
+                    -- mHiddenFileCount;
+                }
+                //m_total_size -= node->size();
+            }
+        }
+        Q_EMIT countDone(mFileCount, mHiddenFileCount, mTotalSize);
+    }
+    Q_EMIT operationPrepared();
+    Q_EMIT operationFinished();
+    for (auto node : nodes) {
+        delete node;
+    }
 }
 
 std::shared_ptr<FileOperationInfo> FileCountOperation::getOperationInfo()
@@ -30,5 +70,6 @@ void FileCountOperation::getInfo(quint64 &file_count, quint64 &hidden_file_count
 
 void FileCountOperation::slotCancel()
 {
-
+    FileOperation::slotCancel();
+    mReporter->slotCancel();
 }
